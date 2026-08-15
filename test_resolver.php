2202827b31e4cea72a6db485ef2c3753063d1094
Family Home Developer <developer@familyhome.local>
2026-08-15 08:34:16 +0300

    chore: remove debug/test artifacts from git tracking (kept locally)

--- test_resolver.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-$svc = app(\App\Domain\Listings\Services\GoogleMapsUrlResolverService::class);
-$res = $svc->resolveAndExtract('https://maps.app.goo.gl/wKkt1cHgW5VZBjTKA');
-var_dump($res);
