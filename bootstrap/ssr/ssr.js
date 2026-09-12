93a2fd64d7a9580e0a619a344ad4922b7405d211
Family Home Developer <developer@familyhome.local>
2026-09-13 02:40:37 +0300

    build: compile assets with sanitized widget message and updated manifests

--- bootstrap/ssr/ssr.js
+++ bootstrap/ssr/ssr.js
@@ -15701,7 +15701,7 @@ function CompareBar() {
 }
 //#endregion
 //#region resources/js/Components/Layout/Footer.jsx
-var HossamChatWidget = lazy(() => import("./assets/HossamChatWidget-BLdzr0jh.js"));
+var HossamChatWidget = lazy(() => import("./assets/HossamChatWidget-CKtGd_ty.js"));
 var QUICK_LINKS = [
 	{
 		key: "home",
